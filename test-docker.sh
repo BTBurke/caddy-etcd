054abeef24788033d389127c71c867edc974b5c9
Bryan Burke <bryan.t.burke@gmail.com>
2019-02-24 16:03:38 -0400

    use pinned version of etcd in tests

--- test-docker.sh
+++ test-docker.sh
@@ -13,5 +13,5 @@ $DOCKER_RUN \
   -p 4001:4001 \
   --name etcd \
   -v /usr/share/ca-certificates/:/etc/ssl/certs \
-  quay.io/coreos/etcd:3.3.12 \
+  quay.io/coreos/etcd:v3.3.12 \
   etcd --listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 --advertise-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001
